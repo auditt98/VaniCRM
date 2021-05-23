4837ca7af434b6b50d442cf53e08449babea4740
Trinh xu <auditt_98@yahoo.com.vn>
2021-05-23 09:41:33 +0700

    .

--- App/Backend/Backend/Domain/CAMPAIGN.cs
+++ App/Backend/Backend/Domain/CAMPAIGN.cs
@@ -26,7 +26,6 @@ namespace Backend.Domain
         [StringLength(100)]
         public string Name { get; set; }
 
-        [StringLength(2000)]
         public string Description { get; set; }
 
         public int? CampaignOwner { get; set; }
