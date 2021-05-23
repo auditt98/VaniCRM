55c8a5f18dc5da2a2dca3c1413c4467d9ded74a0
Trinh xu <auditt_98@yahoo.com.vn>
2021-05-23 09:40:42 +0700

    .

--- Documentation/AddEventId.sql
+++ Documentation/AddEventId.sql
@@ -3,3 +3,11 @@ go
 alter table [TASK_TEMPLATE]
 add EventId nvarchar(2000)
 go
+
+alter table [DEAL]
+add ExpectedClosingDate datetime
+go
+
+alter table [CAMPAIGN]
+alter column Description nvarchar(max)
+go
